bc32695bce415c87a142e62333508e6d07e6871a
Rusty-spotted cat <skwjdgh2@hotmail.com>
2025-09-18 09:33:23 +0900

    Delete func_STT_TTS directory

--- func_STT_TTS/backup/__init__.py
+++
@@ -1,5 +0,0 @@
-from .factory import create_stt, create_tts, create_vad
-from .interface import ISTT, ITTS, IVAD
-from .exceptions import KioskException,TranscriptionError, TTSError, VADStreamError
-
-print("STT_TTS 패키지가 로드되었습니다.")
